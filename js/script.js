73c9eb7a5b1ababa473d0f822174ea76acff51fd
Drew <drew@finstrom.us>
2015-06-01 13:39:16 -0700

    trash branch

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
 
   $("form#addPlace").submit(function(event) {
     event.preventDefault();
+
     var name = $("input#placeName").val();
     $("input#placeName").val('');
     var newPlace = {name: name, landmarks: []};
@@ -12,33 +13,40 @@ $( document ).ready(function() {
     placesList.append("<li><span class='place'>" + newPlace.name + "</span></li>");
 
     $(".place").last().click(function() {
+      reloadPlaceDetails(newPlace);
+    });
+
+    var reloadPlaceDetails = function(place) {
       $(".placeDetail").show();
-      $(".placeDetail h2").text(newPlace.name);
+      $(".placeDetail h2").text(place.name);
 
-      $("form#updatePlace").submit(function(event) {
-        event.preventDefault();
+      $(".placeLocation").show();
+      $(".placeLocation span").text(place.location);
 
-        var location = $("input#placeLocation").val();
-        if(location !== '') {
-          $("input#placeLocation").val('');
-          newPlace['location'] = location;
-          $(".placeLocation").show();
-          $(".placeLocation span").text(newPlace.location);
-        }
-
-        var landmark = $("input#placeLandmark").val();
-        if (landmark !== '') {
-          $("input#placeLandmark").val('');
-
-          newPlace.landmarks.push(landmark);
-          $(".placeLandmarks").show();
-          $(".placeLandmarks ul").empty();
-          newPlace.landmarks.forEach(function (landmark) {
-              $(".placeLandmarks ul").append("<li>" + landmark + "</li>");
-          });
-        }
-
+      $(".placeLandmarks").show();
+      $(".placeLandmarks ul").empty();
+      place.landmarks.forEach(function (landmark) {
+          $(".placeLandmarks ul").append("<li>" + landmark + "</li>");
       });
+    };
+
+    $("form#updatePlace").submit(function(event) {
+      event.preventDefault();
+
+      var location = $("input#placeLocation").val();
+      if(location !== '') {
+        $("input#placeLocation").val('');
+        newPlace['location'] = location;
+      }
+
+      var landmark = $("input#placeLandmark").val();
+      if (landmark !== '') {
+        $("input#placeLandmark").val('');
+
+        newPlace.landmarks.push(landmark);
+      }
+
+      reloadPlaceDetails(newPlace);
 
     });
 
